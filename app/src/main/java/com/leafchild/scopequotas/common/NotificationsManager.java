20fcef0c2e83837858178b4941455a3712041d67
leafchild0 <vmalyshev0@gmail.com>
2018-12-15 18:19:23 +0200

    Fixed the build

--- app/src/main/java/com/leafchild/scopequotas/common/NotificationsManager.java
+++ app/src/main/java/com/leafchild/scopequotas/common/NotificationsManager.java
@@ -4,7 +4,6 @@ import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
